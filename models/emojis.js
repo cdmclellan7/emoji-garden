c00663fadc56a3bf36a94c2eff313d454c86d9df
casey <cassandra.d.mclellan@gmail.com>
2021-12-24 12:38:43 +0000

    get all emojis model

--- models/emojis.js
+++ models/emojis.js
@@ -1,10 +1,16 @@
 import query from '../db/index.js';
 
+/* READ */
+export function getEmojis() {
+    const sqlString = `SELECT * FROM emojis;`;
+    const res = query(sqlString);
+    return res.rows;
+}
 
 
+/* CREATE */
 export function addEmoji(code, xPos, yPos) {
     const sqlString = `INSERT INTO emojis (code, x_position, y_position) VALUES ($1, $2, $3) RETURNING *;`;
-
     const res = query(sqlString, [code, xPos, yPos]);
     return res.rows;
 }
