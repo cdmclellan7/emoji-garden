670adddb9bf1513e79cae2be93a3c944539c2826
casey <cassandra.d.mclellan@gmail.com>
2021-12-24 15:06:02 +0000

    user can plant one type of emoji flower (front-end only).

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
-console.log("hello from main.js")
+const garden = document.querySelector("#garden");
+garden.addEventListener("click", plantSeed);
 
-function test() {
-let x = 1
+function plantSeed(e) {
+    const xPos = e.x;
+    const yPos = e.y;
+
+    addSpan(xPos, yPos);
+}
+
+function addSpan(xPos, yPos) {
+    const newSpan = document.createElement('span');
+    newSpan.classList.add('plant');
+    newSpan.innerHTML = `&#127803;`;
+    const spanHeight = 42;
+    newSpan.style.fontSize = spanHeight + 'px';
+    newSpan.style.position = 'absolute';
+    newSpan.style.left = xPos - (spanHeight / 2) + 'px';
+    newSpan.style.top = yPos - spanHeight + 'px';
+    garden.appendChild(newSpan);
 }
