76459d9d915821ef97b20c22a387726be21010f2
casey <cassandra.d.mclellan@gmail.com>
2021-12-24 12:43:30 +0000

    post /emojis route handler

--- routes/emojis.js
+++ routes/emojis.js
@@ -1,9 +1,21 @@
 import express from "express";
 const router = express.Router();
 
-/* GET emojis listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "here is an emoji!" });
+import { addEmoji } from "../models/emojis.js";
+
+/* READ */
+router.get("/", (req, res) => {
+    res.json({ message: "here is an emoji!" });
+});
+
+/* CREATE */
+router.post("/", async (req, res) => {
+    const { code, xPos, yPos } = req.body;
+    const data = addEmoji(code, xPos, yPos);
+    res.json({
+        success: true,
+        payload: data.rows
+    });
 });
 
 export default router;
