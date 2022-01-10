618abfe971b23eae2586915f6be94cc41ee9431b
casey <cassandra.d.mclellan@gmail.com>
2022-01-10 10:02:24 +0000

    CHORE: prettier formated main.js code

--- public/js/main.js
+++ public/js/main.js
@@ -19,33 +19,33 @@ async function plantSeed(e) {
     const code = seedlingEmojiCode;
 
     const data = {
-            username: username,
-            gardenName: gardenName,
-            code: code,
-            xPos: xPos,
-            yPos: yPos
+        username: username,
+        gardenName: gardenName,
+        code: code,
+        xPos: xPos,
+        yPos: yPos,
     };
-    const res = await fetch('/api/emojis', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
+    const res = await fetch("/api/emojis", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
     });
     const json = await res.json();
     const newEmoji = json.payload;
     renderEmojis(newEmoji);
-    
+
     startGrowing(newEmoji[0].id, e);
 }
 
 function addSpan(id, xPos, yPos, code) {
-    const newSpan = document.createElement('span');
+    const newSpan = document.createElement("span");
     newSpan.id = `emoji-${id}`;
-    newSpan.classList.add('plant');
+    newSpan.classList.add("plant");
     newSpan.innerHTML = `&#${code};`;
-    newSpan.style.fontSize = spanHeight + 'px';
-    newSpan.style.position = 'absolute';
-    newSpan.style.left = xPos - (spanHeight / 2) + 'px';
-    newSpan.style.top = yPos - spanHeight + 'px';
+    newSpan.style.fontSize = spanHeight + "px";
+    newSpan.style.position = "absolute";
+    newSpan.style.left = xPos - spanHeight / 2 + "px";
+    newSpan.style.top = yPos - spanHeight + "px";
     garden.appendChild(newSpan);
 }
 
@@ -66,7 +66,6 @@ function makeEmojiInactive(span) {
     span.removeEventListener("click", handleActiveClick);
 }
 
-
 /* CLEAR AND LOAD GARDEN */
 
 /*
@@ -85,12 +84,12 @@ async function loadGarden(username) {
     const res = await fetch(`/api/emojis/${username}`);
     const json = await res.json();
     const emojis = json.payload;
-    
+
     renderEmojis(emojis);
 }
 
 function renderEmojis(list) {
-    list.forEach( (emoji) => {
+    list.forEach((emoji) => {
         addSpan(emoji.id, emoji.x_position, emoji.y_position, emoji.dec_code);
     });
 }
@@ -101,10 +100,10 @@ function clearOneEmoji(id) {
 }
 
 /* TIMER CONTROLS */
-const pomodoroMinutes = urlParams.get('work-time');
-const breakMinutes = urlParams.get('break-time');
-const pomodoroTime = 7000 //pomodoroMinutes * 60 * 1000;
-const breakTime = 5000 //breakMinutes * 60 * 1000;
+const pomodoroMinutes = urlParams.get("work-time");
+const breakMinutes = urlParams.get("break-time");
+const pomodoroTime = 7000; //pomodoroMinutes * 60 * 1000;
+const breakTime = 5000; //breakMinutes * 60 * 1000;
 
 const countSeconds = pomodoroTime / 1000;
 let currentCountSeconds = countSeconds;
@@ -114,7 +113,6 @@ let timerIDs;
 
 const breakSeconds = breakTime / 1000;
 let currentBreakSeconds = breakSeconds;
-
 
 function startGrowing(emojiID, e) {
     removeTimers();
@@ -129,22 +127,25 @@ function startGrowing(emojiID, e) {
 
 function startTimers(p, emojiID) {
     isTimerFinished = false;
-    const growTimeID = setTimeout( () => {
+    const growTimeID = setTimeout(() => {
         maturePlant(emojiID);
         stopTimer(p, timerID);
-        p.innerText += " Click to start a break."
+        p.innerText += " Click to start a break.";
     }, currentCountSeconds * 1000);
-    const timerID = setInterval(() => decrementTimer(p, currentCountSeconds--), 1000);
+    const timerID = setInterval(
+        () => decrementTimer(p, currentCountSeconds--),
+        1000
+    );
     return [timerID, growTimeID];
 }
 
 async function maturePlant(id) {
-    const data = {code: tomatoEmojiCode};
+    const data = { code: tomatoEmojiCode };
 
     const res = await fetch(`/api/emojis/${id}`, {
-        method: 'PATCH',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
     });
 
     const json = await res.json();
@@ -157,8 +158,8 @@ async function maturePlant(id) {
 function createTimer(e) {
     const timerP = document.createElement("p");
     timerP.classList.add("timer");
-    timerP.style.left = e.pageX + spanHeight + 'px';
-    timerP.style.top = e.pageY - (spanHeight) + 'px';
+    timerP.style.left = e.pageX + spanHeight + "px";
+    timerP.style.top = e.pageY - spanHeight + "px";
     garden.appendChild(timerP);
     return timerP;
 }
@@ -179,7 +180,7 @@ function stopTimer(p, timerID) {
 }
 
 function removeTimers() {
-    document.querySelectorAll('.timer').forEach(p => p.remove());
+    document.querySelectorAll(".timer").forEach((p) => p.remove());
 }
 
 function toggleTimers(p, timerIDs, isBreakTimer, emojiID) {
@@ -193,19 +194,22 @@ function toggleTimers(p, timerIDs, isBreakTimer, emojiID) {
             p.classList.remove("paused");
             //start new timers
             isTimerCounting = !isTimerCounting;
-            return (isBreakTimer ? startBreakTimers(p) : startTimers(p, emojiID));
+            return isBreakTimer ? startBreakTimers(p) : startTimers(p, emojiID);
         }
     }
 }
 
 function startBreakTimers(p) {
     isTimerFinished = false;
-    const seedTimerID = setTimeout( () => {
+    const seedTimerID = setTimeout(() => {
         stopTimer(p, timerID);
-        p.innerText += " Plant a new seed."
+        p.innerText += " Plant a new seed.";
         showSeed();
     }, currentBreakSeconds * 1000);
-    const timerID = setInterval(() => decrementTimer(p, currentBreakSeconds--), 1000);
+    const timerID = setInterval(
+        () => decrementTimer(p, currentBreakSeconds--),
+        1000
+    );
     return [timerID, seedTimerID];
 }
 
@@ -230,8 +234,8 @@ initialSeed.addEventListener("click", stopSeed);
 window.addEventListener("mousemove", moveSeed);
 
 function moveSeed(e) {
-    initialSeed.style.left = e.pageX - (spanHeight / 2) + 'px';
-    initialSeed.style.top = e.pageY - (spanHeight) + 'px';
+    initialSeed.style.left = e.pageX - spanHeight / 2 + "px";
+    initialSeed.style.top = e.pageY - spanHeight + "px";
 }
 
 function stopSeed(e) {
@@ -244,4 +248,4 @@ function showSeed() {
 }
 
 /* TIMER AUDIO */
-const audio = new Audio('../sounds/bell.wav');
+const audio = new Audio("../sounds/bell.wav");
